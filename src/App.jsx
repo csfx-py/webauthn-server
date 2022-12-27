91b6c7bf7df7b8f7483871f506a98d534720c477
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-28 00:24:48 +0530

    fixed backend url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Client from "webauthn/client";
 
 function App() {
   const client = new Client({
-    pathPrefix: "https://https://bewildered-pig-ring.cyclic.app//webauthn",
+    pathPrefix: "https://bewildered-pig-ring.cyclic.app/webauthn",
   });
   const startRegister = async () => {
     await client.register({
